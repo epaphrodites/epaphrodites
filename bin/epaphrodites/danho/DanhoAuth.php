b0c81934b0b8520edecc276cd2931f95c61a890b
Y'srael Aimé N'dri <73212454+ysrael-aime-ndri@users.noreply.github.com>
2024-01-31 14:30:34 +0000

    update #Auth

--- bin/epaphrodites/danho/DanhoAuth.php
+++ bin/epaphrodites/danho/DanhoAuth.php
@@ -17,23 +17,28 @@ class DanhoAuth extends StartUsersSession
   private function getUsersAuthManagers(string $login, string $usersPassword):bool
   {
 
+    if(!empty($login)&&!empty($usersPassword)){
+
     if ((static::class('verify')->onlyNumberAndCharacter($login, 12)) === false) {
 
       $result = static::getGuard('sql')->checkUsers($login);
 
       if (!empty($result)) {
 
-        if (static::getGuard('guard')->AuthenticatedPassword($result[0]["userspwd"], $usersPassword) === true && $result[0]["usersstat"] === 1) {
-          
-          $this->StartUsersSession($result[0]["idusers"], $result[0]["loginusers"], $result[0]["nomprenomsusers"], $result[0]["contactusers"], $result[0]["emailusers"], $result[0]["typeusers"]);
-          return true;
+          if (static::getGuard('guard')->AuthenticatedPassword($result[0]["userspwd"], $usersPassword) === true && $result[0]["usersstat"] === 1) {
+            
+            $this->StartUsersSession($result[0]["idusers"], $result[0]["loginusers"], $result[0]["nomprenomsusers"], $result[0]["contactusers"], $result[0]["emailusers"], $result[0]["typeusers"]);
+            return true;
+          } else {
+            return false;
+          }
         } else {
           return false;
         }
       } else {
         return false;
       }
-    } else {
+    }else{
       return false;
     }
   }
