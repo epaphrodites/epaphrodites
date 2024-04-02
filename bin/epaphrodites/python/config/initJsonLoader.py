eaaa1dfda33998cc01b328d708788a2a8144c0f9
Y'srael Aimé N'dri <73212454+ysrael-aime-ndri@users.noreply.github.com>
2024-04-02 06:34:02 +0000

    Update initJsonLoader.py

--- bin/epaphrodites/python/config/initJsonLoader.py
+++ bin/epaphrodites/python/config/initJsonLoader.py
@@ -1,23 +1,29 @@
 import json
+import re
 
 class InitJsonLoader:
     
     @staticmethod
     def add_quotes(string):
         result = []
-        elements = string.strip('{}').split(',')
+        elements = re.findall(r'([^:]+):(".*?"|[^,]+)', string.strip('{}'))
         for element in elements:
-            key, value = element.split(':')
-            if not key.startswith('"'): key = '"' + key
-            if not key.endswith('"'): key = key + '"'
-            if not value.startswith('"') and value not in ['true', 'false', 'null']: value = '"' + value
-            if not value.endswith('"') and value not in ['true', 'false', 'null']: value = value + '"'
+            key, value = element
+            if not key.startswith('"') and not key.endswith('"'):
+                key = '"' + key.strip() + '"'
+
+            if value.startswith('"') and value.endswith('"'):
+                pass  
+            elif value in ['true', 'false', 'null']:
+                pass
+            else:
+                value = '"' + value.strip() + '"'
             result.append(key + ':' + value)
-        return '{' + ','.join(result) + '}'
+        return '{' + ''.join(result) + '}'
 
     @staticmethod
     def loadJsonValues(json_values):
 
         json_values = InitJsonLoader.add_quotes(json_values)
-        values = json.loads(json_values)
-        return values
+       # values = json.loads(json_values)
+        return json_values
