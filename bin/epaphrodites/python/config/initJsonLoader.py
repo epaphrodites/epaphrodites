00adba3f3227275cd4318e3ff424dd90e783de44
Y'srael Aimé N'dri <73212454+ysrael-aime-ndri@users.noreply.github.com>
2024-04-02 07:39:36 +0000

    Update initJsonLoader.py

--- bin/epaphrodites/python/config/initJsonLoader.py
+++ bin/epaphrodites/python/config/initJsonLoader.py
@@ -5,22 +5,38 @@ class InitJsonLoader:
     
     @staticmethod
     def add_quotes(string):
-        clean_string = re.sub(r'\s*"\s*', '"', string)
-    
-        # Utiliser une expression régulière pour extraire les paires clé-valeur
-        pairs = re.findall(r'"([^"]+)":"([^"]+)"', clean_string)
+        # Supprimer les accolades initiales et finales
+        string = string.strip('{}')
         
-        # Construire un dictionnaire à partir des paires extraites
-        data = {key: value for key, value in pairs}
+        # Trouver toutes les paires clé-valeur
+        pairs = re.findall(r'(\w+): ([\w]+)', string)
+        
+        # Créer un dictionnaire avec les clés et valeurs correctes
+        data = {}
+        for key, value in pairs:
+            # Traiter les valeurs spéciales (true, false, null)
+            if value == 'true':
+                processed_value = True
+            elif value == 'false':
+                processed_value = False
+            elif value == 'null':
+                processed_value = None
+            else:
+                # Traiter les autres valeurs comme des chaînes
+                processed_value = value
+                
+            data[key] = processed_value
         
         # Convertir le dictionnaire en chaîne JSON
-        json_string = json.dumps(data)
+        json_string = json.dumps(data, ensure_ascii=False)
         
         return json_string
 
     @staticmethod
     def loadJsonValues(json_values):
 
-        json_values = InitJsonLoader.add_quotes(json_values)
+       # json_values = InitJsonLoader.add_quotes(json_values)
+        
+       # json_values = json.loads(json_values)
         
         return json_values
