ac3776663106699798364cbacd4939eb2550ecf2
Y'srael Aimé N'dri <73212454+ysrael-aime-ndri@users.noreply.github.com>
2024-04-02 07:28:05 +0000

    Update initJsonLoader.py

--- bin/epaphrodites/python/config/initJsonLoader.py
+++ bin/epaphrodites/python/config/initJsonLoader.py
@@ -5,38 +5,22 @@ class InitJsonLoader:
     
     @staticmethod
     def add_quotes(string):
-        result = []
-        elements = re.findall(r'([^:]+):(".*?"|[^,]+)', string.strip('{}'))
-        for i, element in enumerate(elements):
-            key, value = element
-            key = re.sub(r'(^")|("$)', '', key)  # Supprimer les guillemets doubles de début et de fin
-            value = re.sub(r'(^")|("$)', '', value)  # Supprimer les guillemets doubles de début et de fin
-
-            if not key.startswith('"') and not key.endswith('"'):
-                key = '"' + key.strip() + '"'
-
-            if value in ['true', 'false', 'null']:
-                pass  # La valeur est une constante JSON, ne pas l'entourer de guillemets
-            elif value.startswith('"') and value.endswith('"'):
-                pass  # La valeur est déjà entourée de guillemets doubles
-            else:
-                value = '"' + value.strip() + '"'
-
-            if i == 0:
-                result.append(key + ':' + value)
-            else:
-                previous_value_quoted = result[-1].endswith('"')
-                current_value_quoted = value.startswith('"') and value.endswith('"')
-                if previous_value_quoted or current_value_quoted:
-                    result.append(',' + key + ':' + value)
-                else:
-                    result.append(key + ':' + value)
-
-        return '{' + ''.join(result) + '}'
+        clean_string = re.sub(r'\s*"\s*', '"', string)
+    
+        # Utiliser une expression régulière pour extraire les paires clé-valeur
+        pairs = re.findall(r'"([^"]+)":"([^"]+)"', clean_string)
+        
+        # Construire un dictionnaire à partir des paires extraites
+        data = {key: value for key, value in pairs}
+        
+        # Convertir le dictionnaire en chaîne JSON
+        json_string = json.dumps(data)
+        
+        return json_string
 
     @staticmethod
     def loadJsonValues(json_values):
 
         json_values = InitJsonLoader.add_quotes(json_values)
-       # values = json.loads(json_values)
+        
         return json_values
