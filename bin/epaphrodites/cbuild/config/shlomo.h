9463b1237ae5abca2ac845926ff25b2b21a775f4
Y'srael Aimé N'dri <73212454+ysrael-aime-ndri@users.noreply.github.com>
2025-04-17 05:33:42 +0000

    update

--- bin/epaphrodites/cbuild/config/shlomo.h
+++ bin/epaphrodites/cbuild/config/shlomo.h
@@ -9,22 +9,20 @@ extern zend_module_entry shlomo_module_entry;
 #include "TSRM.h"
 #endif
 
-// Si tu ajoutes des globals plus tard
 ZEND_BEGIN_MODULE_GLOBALS(shlomo)
- // long example_global;
+
 ZEND_END_MODULE_GLOBALS(shlomo)
 
-// Accès aux globals
 #ifdef ZTS
 #define SHLOMO_G(v) TSRMG(shlomo_globals_id, zend_shlomo_globals *, v)
 #else
 #define SHLOMO_G(v) (shlomo_globals.v)
 #endif
 
-// Déclarations des fonctions exposées à PHP
+// Declare your PHP functions here
 PHP_FUNCTION(shlomo_hello);
 PHP_FUNCTION(shlomo_factorial);
 PHP_FUNCTION(shlomo_reverse_string);
 PHP_FUNCTION(shlomo_file_stats);
 
-#endif /* PHP_SHLOMO_H */
+#endif
--- bin/epaphrodites/cbuild/stubs/shlomo.stub.php
+++ bin/epaphrodites/cbuild/stubs/shlomo.stub.php
@@ -1,5 +1,8 @@
 <?php
 
+// ========================================================================
+// Enable Intelephense support for your native extension functions
+// ========================================================================
 
 function shlomo_hello() {}
 
